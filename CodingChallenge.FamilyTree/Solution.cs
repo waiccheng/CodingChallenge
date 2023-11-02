63a66acdf24423b8a38d3faea0c1b5ef0f3e8a3f
waiccheng <wai.c.cheng@gmail.com>
2023-11-02 12:49:00 -0500

    got FamilyTree working

--- CodingChallenge.FamilyTree/Solution.cs
+++ CodingChallenge.FamilyTree/Solution.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CodingChallenge.FamilyTree
 {
@@ -6,7 +7,34 @@ namespace CodingChallenge.FamilyTree
     {
         public string GetBirthMonth(Person person, string descendantName)
         {
-            throw new NotImplementedException();
+            string birthMonth = "";
+            List<Person> children = new List<Person>();
+            if (person.Name == descendantName)
+            { //it could be him or her self
+                return person.Birthday.ToString("MMMM");
+            }
+            foreach (Person descendant in person.Descendants)
+            {
+                if (descendant.Name == descendantName)
+                {
+                    birthMonth = descendant.Birthday.ToString("MMMM");
+                    break;
+                }
+                if (descendant.Descendants.Count > 0) //if child has children add to the list of children
+                    children.AddRange(descendant.Descendants);
+            }
+
+            if (birthMonth == "")
+            {
+                foreach (Person descendant in children)
+                {
+                    birthMonth = GetBirthMonth(descendant, descendantName); //check the children 
+                    if (birthMonth != "") break; // this will break recursive function
+                }
+            }
+
+            return birthMonth;
+
         }
     }
 }
