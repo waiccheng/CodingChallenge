5286761a0c8f4041e83a5a73c02728368573d84e
waiccheng <wai.c.cheng@gmail.com>
2023-11-02 15:10:50 -0500

    Got Pirate Speak working

--- CodingChallenge.PirateSpeak/Solution.cs
+++ CodingChallenge.PirateSpeak/Solution.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace CodingChallenge.PirateSpeak
@@ -7,7 +8,43 @@ namespace CodingChallenge.PirateSpeak
     {
         public string[] GetPossibleWords(string jumble, string[] dictionary)
         {
-            throw new NotImplementedException();
+            var listOfStrings = new List<string>();
+
+            foreach (string word in dictionary)
+            {
+                if (MatchWord(jumble, word))
+                {
+                    listOfStrings.Add(word);
+                }
+            }
+
+            string[] arrayOfStrings = listOfStrings.ToArray();
+            return arrayOfStrings;
+        }
+
+        private bool MatchWord(string jumble, string word)
+        {
+            var match = false;
+            var listOfCharMatchPos = new List<int>();
+
+            if (jumble.Length != word.Length)
+                return false;
+
+            foreach (char jchar in jumble)
+            {
+                for (int i = 0; i < word.Length; i++)
+                {
+                    if (!listOfCharMatchPos.Contains(i) && jchar == word[i])
+                    {
+                        listOfCharMatchPos.Add(i);
+                        break;
+                    }
+                }
+            }
+            if (jumble.Length == listOfCharMatchPos.Count)
+                match = true;
+
+            return match;
         }
     }
 }
